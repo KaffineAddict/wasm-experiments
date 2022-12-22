b8ce77d8df07b3321f8ca8939d1164220ec6ca05
Blake Sutton <sutton.blake@gmail.com>
2022-12-21 19:28:01 -0700

    added root dotnet

---
+++ lib/dotnet/HelloWasm/HelloDotnet/Library.cs
@@ -0,0 +1,18 @@
+﻿namespace HelloDotnet;
+public class Library
+{
+    [System.Runtime.InteropServices.UnmanagedCallersOnly(EntryPoint = "hello")]
+    public static int Hello()
+    {
+
+        return 42;
+    }
+
+    [System.Runtime.InteropServices.UnmanagedCallersOnly(EntryPoint = "add")]
+    public static int Add(int x, int y)
+    {
+        return x+y;
+    }
+}
+
+//dotnet publish /p:NativeLib=Static /p:SelfContained=true -r browser-wasm -c Debug /p:TargetArchitecture=wasm /p:PlatformTarget=AnyCPU /p:MSBuildEnableWorkloadResolver=false /p:EmccExtraArgs="-s EXPORTED_FUNCTIONS=_add%2C_hello -s EXPORTED_RUNTIME_METHODS=cwrap" --self-contained
---
+++ lib/dotnet/HelloWasm/HelloWorld/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        [System.Runtime.InteropServices.UnmanagedCallersOnly(EntryPoint = "hello")]
+        public static int Hello()
+        {
+            return 42;
+        }
+
+        [System.Runtime.InteropServices.UnmanagedCallersOnly(EntryPoint = "add")]
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
